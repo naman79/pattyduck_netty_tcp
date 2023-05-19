4520333aa4929770c8a333d0ceda5950a685a2fc
강민수 <naman79@naver.com>
2023-05-19 17:37:11 +0900

    Add netty server
    set package

---
+++ src/main/java/com/nettytcp/NettytcpApplication.java
@@ -0,0 +1,13 @@
+package com.nettytcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettytcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NettytcpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nettytcp/NettytcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nettytcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettytcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
